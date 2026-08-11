517a5dddf1b716ecf4a7529251fa91b36e58efd9
ninglong77 <1936855897@qq.com>
2026-08-11 23:12:31 +0800

    update

--- src/components/editor/shot_elements.tsx
+++ src/components/editor/shot_elements.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
-import { ActionIcon, BackgroundIcon, DialogIcon, RoleIcon, ShotIcon } from "../icons";
+// import { ActionIcon, BackgroundIcon, DialogIcon, RoleIcon, ShotIcon } from "../icons";
 
-
+/**
+ * 分镜头元素
+ * @param props 
+ * @returns 
+ */
 export const ShotElement = (props: any) => {
   const node = props.element as any;
   const [content, setContent] = useState("");
@@ -15,9 +19,9 @@ export const ShotElement = (props: any) => {
   }, [content])
   return (
     <div className="border border-slate-100 bg-white relative shadow-sm" {...props.attributes}>
-      <div style={{left: -25, zIndex: 50, top: '5%'}} className="absolute bg-white border-l border-slate-300 rounded-l-2xl p-1">
+      {/* <div style={{left: -25, zIndex: 50, top: '5%'}} className="absolute bg-white border-l border-slate-300 rounded-l-2xl p-1">
         <ShotIcon />
-      </div>
+      </div> */}
       {/* <div>Shot {content}</div> */}
       <div className="p-2">
         {props.children}
@@ -26,37 +30,61 @@ export const ShotElement = (props: any) => {
   );
 }
 
+
+/**
+ * 角色元素
+ * @param props 
+ * @returns 
+ */
 export const RoleElement = (props: any) => {
   return (
     <span className="text-yellow-500 px-1 font-bold" {...props.attributes}>
-      <span style={{height: 16, width: 16}} className="inline-block"><RoleIcon /></span>
+      {/* <span style={{height: 16, width: 16}} className="inline-block"><RoleIcon /></span> */}
       {props.children}
     </span>
   );
 }
 
+
+/**
+ * 动作元素
+ * @param props 
+ * @returns 
+ */
 export const ActionElement = (props: any) => {
   return (
     <span className="text-green-500 px-1" {...props.attributes}>
-      <span style={{height: 16, width: 16}} className="inline-block"><ActionIcon /></span>
+      {/* <span style={{height: 16, width: 16}} className="inline-block"><ActionIcon /></span> */}
       {props.children}
     </span>
   );
 }
 
+
+/**
+ * 背景元素
+ * @param props 
+ * @returns 
+ */
 export const BackgroundElement = (props: any) => {
   return (
     <span className="text-blue-500 px-1" {...props.attributes}>
-      <span style={{height: 16, width: 16}} className="inline-block"><BackgroundIcon /></span>
+      {/* <span style={{height: 16, width: 16}} className="inline-block"><BackgroundIcon /></span> */}
       {props.children}
     </span>
   );
 }
 
+
+/**
+ * 对话元素
+ * @param props 
+ * @returns 
+ */
 export const DialogElement = (props: any) => {
   return (
     <span className="text-pink-500 px-1" {...props.attributes}>
-      <span style={{height: 16, width: 16}} className="inline-block"><DialogIcon /></span>
+      {/* <span style={{height: 16, width: 16}} className="inline-block"><DialogIcon /></span> */}
       {props.children}
     </span>
   );
